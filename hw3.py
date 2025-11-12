3df059688505e29b57821bae65ee348a8f73dc2c
Zahar <zakhar.zakharchenko0011@gmail.com>
2025-11-12 22:34:12 +0200

    hw3

--- hw3.py
+++ hw3.py
@@ -1,40 +1,48 @@
 class Product:
-    def __init__(self,price,quantity,bestbeforedate):
+    def __init__(self,name,price,quantity,bestbeforedate):
+        self.name=name
         self.price = price
         self.quantity = quantity
         self.bestbeforedate = bestbeforedate
     def print_info(self):
-        print(f':{self.price})')
-        print(f':{self.quantity})')
-        print(f':{self.bestbeforedate})')
-Spagetti=Product("1 euro","5","28.04.2027")
+        print(f'name:{self.name}')
+        print(f'price:{self.price}')
+        print(f'quantity:{self.quantity}')
+        print(f'bestbefordate:{self.bestbeforedate}')
+Spagetti=Product("Spagetti","1 euro","5","28.08.2026")
+print(f'Spagetti name: {Spagetti.name}')
 print(f'Spagetti price: {Spagetti.price}')
 print(f'Spagetti quantity: {Spagetti.quantity}')
 print(f'Spagetti bestbeforedate: {Spagetti.bestbeforedate}')
-Tomatos=Product("0.8 euro","10","5.12.2025")
+Tomatos=Product("Tomato","0.8 euro","2","30.11.2025")
+print(f'Tomatos name: {Tomatos.name}')
 print(f'Tomatos price: {Tomatos.price}')
 print(f'Tomatos quantity: {Tomatos.quantity}')
 print(f'Tomatos bestbeforedate: {Tomatos.bestbeforedate}')
-Sweets=Product("1.20 euro","2","7.01.2025")
+Sweets=Product("Sweets","1.20","10","01.01.2026")
+print(f'Sweets name: {Sweets.name}')
 print(f'Sweets price: {Sweets.price}')
 print(f'Sweets quantity: {Sweets.quantity}')
 print(f'Sweets bestbeforedate: {Sweets.bestbeforedate}')
-Pivo=Product("1.30 euro","0","20.01.2025")
+Pivo=Product("Pivo","1.30","0","16.04.2026")
+print(f'Pivo name: {Pivo.name}')
 print(f'Pivo price: {Pivo.price}')
 print(f'Pivo quantity: {Pivo.quantity}')
 print(f'Pivo bestbeforedate: {Pivo.bestbeforedate}')
-Milk=Product("0.90 euro","1","20.11.2025")
+Milk=Product("Milk","0.60","2","26.11.2025")
+print(f'Milk name: {Milk.name}')
 print(f'Milk price: {Milk.price}')
 print(f'Milk quantity: {Milk.quantity}')
 print(f'Milk bestbeforedate: {Milk.bestbeforedate}')
 class Cart:
     def __init__(self):
         self.buy=[]
-    def add(self):
-        name_input = input(' Name of the product ')
-        quantity_input = input(' Quantity of the product ')
-        price_input = input(' Price of the product ')
-        bestbeforedate_input = input(' Best beforedate of the product ')
+    def add(self,good):
+       self.buy.append(good)
+        # self.name_input = input(' Name of the product ')
+        # self.quantity_input = input(' Quantity of the product ')
+        # self.price_input = input(' Price of the product ')
+        # self.bestbeforedate_input = input(' Best beforedate of the product ')
 
 
 
@@ -45,7 +53,8 @@ Cart=Cart()
 while True:
     choose = input(f'Menu:\n[1]-add product\n[2]-display all products\n[3]-exit\nEnter your choice: ')
     if choose == '1':
-        Cart.add()
+        product=Product(input('price:'),input('quantity:'),input('bestbeforedate:'),input('name:'))
+        Cart.add(product)
     if choose == '2':
         Cart.print_all_products()
     if choose == '3':
